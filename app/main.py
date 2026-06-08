5614f7601b30df05f5d3d1803f67aacb2061ffae
Ivan Coelho <67839678+ivancda@users.noreply.github.com>
2026-06-08 14:02:04 -0300

    feat: DB mount

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from app.api.review import router as review_router
+from app.core.database import Base, engine
+from app.models.review import Review
+
+Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 app.include_router(review_router)
