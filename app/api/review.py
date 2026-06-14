bb377e6ec410f69866604009beeb9b2d7bd0cb14
Ivan Coelho <67839678+ivancda@users.noreply.github.com>
2026-06-14 12:14:06 -0300

    fix: response model no get, response model no decorator das rotas

--- app/api/review.py
+++ app/api/review.py
@@ -1,17 +1,17 @@
 from app.core.database import get_db
-from app.schemas.review import ReviewRequest, ReviewResponse
+from app.schemas.review import ReviewRequest, ReviewResponse, ReviewDB
 from app.services.review_service import review_code, get_all_reviews
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 router = APIRouter( )
 
-@router.post("/review")
-def review_endpoint(review_request: ReviewRequest, db: Session = Depends(get_db)) -> ReviewResponse:
+@router.post("/review", response_model=ReviewResponse)
+def review_endpoint(review_request: ReviewRequest, db: Session = Depends(get_db)):
     resultado = review_code(review_request, db)
     return ReviewResponse(status="success", review=resultado.result)
 
-@router.get("/review")
+@router.get("/review", response_model=list[ReviewDB])
 def get_reviews(db: Session = Depends(get_db)):
     reviews = get_all_reviews(db)
     return reviews
--- app/schemas/review.py
+++ app/schemas/review.py
@@ -1,9 +1,20 @@
 # lib pydantic serve para criar modelo de dados (schemas)
 from pydantic import BaseModel
 
+from datetime import datetime
+
 class ReviewRequest(BaseModel):
     code: str
 
 class ReviewResponse(BaseModel):
     status: str
     review: str
+
+class ReviewDB(BaseModel):
+    id: int
+    code: str
+    result: str
+    created_at: datetime
+
+    # from_attributes = True: diz ao Pydantic para ler os atributos do objeto SQLAlchemy.
+    model_config = {"from_attributes": True}
