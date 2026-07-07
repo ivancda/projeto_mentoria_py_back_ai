6b42372b7d8a8b99489c0fa4e71ca3194e5a66a4
Ivan Coelho <67839678+ivancda@users.noreply.github.com>
2026-07-07 11:59:00 -0300

    fix: service is now using llms by provider layer

--- app/services/review_service.py
+++ app/services/review_service.py
@@ -2,9 +2,11 @@ from sqlalchemy.orm import Session
 from app.models.review import Review
 from app.schemas.review import ReviewRequest, ReviewResponse
 from app.repositories.review_repository import ReviewRepository
+from app.providers.provider_factory import get_provider
 
 def review_code(review_request: ReviewRequest, db: Session) -> ReviewResponse:
-    result = f"review realizada para o código: {review_request.code}"
+    provider = get_provider()
+    result = provider.review(review_request.code)
     repository = ReviewRepository(db)
     review_saved = repository.create(code=review_request.code, result=result)
     return ReviewResponse(status="success", review=review_saved.result)
