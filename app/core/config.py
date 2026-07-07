52ec013b5bc1a1eaa0a617aa055543fc42d9442d
Ivan Coelho <67839678+ivancda@users.noreply.github.com>
2026-07-07 11:57:36 -0300

    chore: new env vars

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+    OLLAMA_URL: str
+    OLLAMA_MODEL: str
+    LLM_PROVIDER: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
